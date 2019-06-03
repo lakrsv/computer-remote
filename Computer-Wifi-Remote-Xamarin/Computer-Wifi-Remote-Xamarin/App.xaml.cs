d21c4808b91c8761ff3100721d06d3b3c187f0c8
Lars <lakrsv@gmail.com>
2019-06-03 21:47:58 +0100

    Added template xamarin project

---
+++ Computer-Wifi-Remote-Xamarin/Computer-Wifi-Remote-Xamarin/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Computer_Wifi_Remote_Xamarin.Services;
+using Computer_Wifi_Remote_Xamarin.Views;
+
+namespace Computer_Wifi_Remote_Xamarin
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ Computer-Wifi-Remote-Xamarin/Computer-Wifi-Remote-Xamarin/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Computer_Wifi_Remote_Xamarin.Models;
+
+namespace Computer_Wifi_Remote_Xamarin.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
