5818669928ae6abe661b90793c2a544e0502d331
Lars <lakrsv@gmail.com>
2019-07-29 18:38:53 +0100

    Added hiding console window

--- Server/Server/Server.cs
+++ Server/Server/Server.cs
@@ -1,4 +1,6 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
 using WebSocketSharp.Server;
 
 namespace Computer_Wifi_Remote.Server
@@ -12,6 +14,8 @@ namespace Computer_Wifi_Remote.Server
             webSocketServer = new WebSocketServer("ws://192.168.1.113:34198");
             webSocketServer.AddWebSocketService<CommandService>("/command");
             webSocketServer.Start();
+
+            ShowWindow(GetConsoleWindow(), SW_HIDE);
         }
 
         public void Start()
@@ -23,5 +27,14 @@ namespace Computer_Wifi_Remote.Server
         {
             webSocketServer.Stop();
         }
+
+        [DllImport("kernel32.dll")]
+        static extern IntPtr GetConsoleWindow();
+
+        [DllImport("user32.dll")]
+        static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        const int SW_HIDE = 0;
+        const int SW_SHOW = 5;
     }
 }
